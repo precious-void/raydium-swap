1fed469e42c4c3ee9bbb08e910df6038234313f8
Artem Stelzer <20266461+shtelzerartem@users.noreply.github.com>
2024-01-28 21:45:06 +0200

    Add implementation with versioned transaction

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,45 @@
-import RaydiumSwap from "./RaydiumSwap"
+import RaydiumSwap from './RaydiumSwap'
 
 const swap = async () => {
-    const executeSwap = false; // Change to true to execute swap
-    const tokenAAmount = 0.01; // e.g. 0.01 SOL -> B_TOKEN
+  const executeSwap = false // Change to true to execute swap
+  const useVersionedTransaction = true // Use versioned transaction
+  const tokenAAmount = 0.01 // e.g. 0.01 SOL -> B_TOKEN
 
-    const tokenAAddress = 'So11111111111111111111111111111111111111112';    // e.g. SOLANA mint address
-    const tokenBAddress = 'HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3';   // e.g. PYTH mint address
+  const tokenAAddress = 'So11111111111111111111111111111111111111112' // e.g. SOLANA mint address
+  const tokenBAddress = 'HZ1JovNiVvGrGNiiYvEozEVgZ58xaU3RKwX8eACQBCt3' // e.g. PYTH mint address
 
-    const raydiumSwap = new RaydiumSwap(process.env.RPC_URL, process.env.WALLET_PRIVATE_KEY);
-    console.log(`Raydium swap initialized`);
+  const raydiumSwap = new RaydiumSwap(process.env.RPC_URL, process.env.WALLET_PRIVATE_KEY)
+  console.log(`Raydium swap initialized`)
 
-    // Loading with pool keys from https://api.raydium.io/v2/sdk/liquidity/mainnet.json
-    await raydiumSwap.loadPoolKeys();
-    console.log(`Loaded pool keys`);
+  // Loading with pool keys from https://api.raydium.io/v2/sdk/liquidity/mainnet.json
+  await raydiumSwap.loadPoolKeys()
+  console.log(`Loaded pool keys`)
 
+  // Trying to find pool info in the json we loaded earlier and by comparing baseMint and tokenBAddress
+  const poolInfo = raydiumSwap.findPoolInfoForTokens(tokenAAddress, tokenBAddress)
+  console.log('Found pool info')
 
-    // Trying to find pool info in the json we loaded earlier and by comparing baseMint and tokenBAddress
-    const poolInfo = raydiumSwap.findPoolInfoForTokens(tokenAAddress, tokenBAddress);
-    console.log('Found pool info')
+  const [txLegacy, txVersioned] = await raydiumSwap.getSwapTransaction(
+    tokenAAddress,
+    tokenBAddress,
+    tokenAAmount,
+    poolInfo,
+    100000 // Max amount of lamports
+  )
 
-    const tx = await raydiumSwap.getSwapTransaction(
-        tokenAAddress,
-        tokenBAddress,
-        tokenAAmount,
-        poolInfo,
-        100000 // Max amount of lamports
-    )
+  if (executeSwap) {
+    const txid = useVersionedTransaction
+      ? await raydiumSwap.sendVersionedTransaction(txVersioned)
+      : await raydiumSwap.sendLegacyTransaction(txLegacy)
 
-    if (executeSwap) {
-        const txid = await raydiumSwap.sendTransaction(tx);
-        console.log(`https://solscan.io/tx/${txid}`);
-    } else {
-        const simRes = await raydiumSwap.simulateTransaction(tx);
-        console.log(simRes)
-    }
+    console.log(`https://solscan.io/tx/${txid}`)
+  } else {
+    const simRes = useVersionedTransaction
+      ? await raydiumSwap.simulateVersionedTransaction(txVersioned)
+      : await raydiumSwap.simulateLegacyTransaction(txLegacy)
+
+    console.log(simRes)
+  }
 }
 
-swap();
+swap()
