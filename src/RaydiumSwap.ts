d972eb3ebd3c5a73c2552ef238545e6005cd6d25
Artem Stelzer <20266461+shtelzerartem@users.noreply.github.com>
2024-03-09 19:35:30 +0200

    Remove maxRetries

--- src/RaydiumSwap.ts
+++ src/RaydiumSwap.ts
@@ -120,7 +120,6 @@ class RaydiumSwap {
   async sendLegacyTransaction(tx: Transaction) {
     const txid = await this.connection.sendTransaction(tx, [this.wallet.payer], {
       skipPreflight: true,
-      maxRetries: 2,
     })
 
     return txid
@@ -129,7 +128,6 @@ class RaydiumSwap {
   async sendVersionedTransaction(tx: VersionedTransaction) {
     const txid = await this.connection.sendTransaction(tx, {
       skipPreflight: true,
-      maxRetries: 2,
     })
 
     return txid
